1642240703f17ade984bc71544aca836050b2b13
野声 <lijiacheng.ljc@antgroup.com>
2022-05-05 19:19:27 +0800

    Revert "feat: add `Domain` deco (#6)"
    This reverts commit 39ca9c4b2b17bb0410c7c5c15f7fdb9f73f0092e.

--- src/declare.ts
+++ src/declare.ts
@@ -1,6 +1,3 @@
-import type { Domain } from './domain';
-export { Domain };
-
 // 构造函数类型
 export type ConstructorOf<T = any> = new (...args: any[]) => T;
 export type TokenResult<T extends Token> = T extends ConstructorOf<infer R> ? R : any;
@@ -9,6 +6,7 @@ export type TokenResult<T extends Token> = T extends ConstructorOf<infer R> ? R 
 // eslint-disable-next-line
 export type Token = string | symbol | Function;
 export type Tag = string | number;
+export type Domain = string | symbol;
 
 // 获取注射器自身的 Token
 export const INJECTOR_TOKEN: Token = Symbol('INJECTOR_TOKEN');
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './declare';
 export * from './decorator';
 export * from './injector';
-export * from './domain';
 export { markInjectable, setParameters, getInjectableOpts } from './helper';
