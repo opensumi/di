58735b0a596dec387017f9d6f85ec57d84bc85fd
野声 <lijiacheng.ljc@antgroup.com>
2022-05-30 23:11:27 +0800

    chore: update versionUpdater

--- scripts/versionUpdater.js
+++ scripts/versionUpdater.js
@@ -1,13 +1,19 @@
+// 替换 ./src/constants.ts 中的版本号
+// 使用的是 standard-version 提供的能力
+
+// 匹配版本号的正则表达式
+const REGEX = /(?<=const VERSION = ')(.*)(?=';)/g;
+
 module.exports.readVersion = function (contents) {
   console.log('read file', contents);
-  const version = contents.match(/(?<=const VERSION = ')(.*)(?=';)/g)[0];
+  const version = contents.match(REGEX)[0];
   console.log('return version', version);
   return version;
 };
 
 module.exports.writeVersion = function (contents, version) {
   console.log('write file', contents);
-  return contents.replace(/(?<=const VERSION = ')(.*)(?=';)/g, () => {
+  return contents.replace(REGEX, () => {
     console.log('replace version with', version);
     return version;
   });
